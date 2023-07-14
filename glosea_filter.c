ee65f82c77a9b855f6d34d956aea06b28e1cd58a
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2023-07-14 16:51:09 +0900

    update the code

--- glosea_filter.c
+++ glosea_filter.c
@@ -7,18 +7,22 @@
 
 #define FILE_NAME "cplfco.1d.mersea.grid_U.nc"
 #define NEW_FILE_NAME "cplfco.1d.mersea.new_grid_U.nc"
+#define SZ_FILTER 32017
 #define DIM1 1000
 #define DIM2 1000
 
 int main(void) {
   int status;
   int ncid, new_ncid, nvars = 0, vartype;
-  std::vector<int> varids, vartypes;
+  std::vector<int> vartypes, ndims;
+  std::vector<std::vector<int>> ndimlens;
+
   std::vector<int> dimids(2);
   std::vector<unsigned int> params;
-  unsigned int filter_id = 32017; // Set actual filter ID here
-  std::vector<size_t> start = {0, 0};
-  std::vector<size_t> count = {DIM1, DIM2};
+  // std::vector<size_t> start = {0, 0};
+  // std::vector<size_t> count = {DIM1, DIM2};
+  std::vector<size_t> start;
+  std::vector<size_t> count;
 
   std::vector<std::vector<float>> data(DIM1, std::vector<float>(DIM2));
 
@@ -45,79 +49,74 @@ int main(void) {
       return 1;
     }
 
-    if ((status = nc_inq_ndims(ncid, &ndim))) {
+    int ndim = 0;
+    int dimids[6];
+    if ((status = nc_inq_varndims(ncid, i, &ndim)) ||
+        (status = nc_inq_vardimid(ncid, i, dimids))) {
       printf("Error getting number of dimensions: %s\n", nc_strerror(status));
       return 1;
     }
 
+    size_t dimlen = 0;
+    std::vector<int> temp;
+    for (int j = 0; j < ndim; j++) {
+      if ((status = nc_inq_dimlen(ncid, dimids[j], &dimlen))) {
+        printf("Error getting number of dimensions: %s\n", nc_strerror(status));
+        return 1;
+      }
+      temp.push_back(dimlen);
+      ndimlens.push_back(temp);
+    }
+
+    ndims.push_back(ndim);
     vartypes.push_back(vartype);
   }
 
-  // for (int i = 0; i < nvars; i++) {
-  //   printf("Loop %d\n", i);
-  //   // Get the variable id
-  //   if ((status = nc_inq_vartype(ncid, i, &datatypes[i]))) {
-  //     printf("Error getting datatype id: %d, %s\n", i, nc_strerror(status));
-  //     return 1;
-  //   }
-  //   printf("datatype: %d\n", datatypes[i]);
+  // TODO: Read the data
+
+  if ((status = nc_close(ncid))) {
+    printf("Error closing file: %s\n", nc_strerror(status));
+    return 1;
+  }
+
+  for (int i = 0; i < nvars; i++) {
+    printf("%d ndim: %d\n", i, ndims[i]);
+    printf("%d vartype: %d\n", i, vartypes[i]);
+    printf("Dim: ");
+    for (int dim : ndimlens[i]) {
+      printf("%d\t", dim);
+    }
+    printf("\n");
+  }
+
+  // Create the new file
+  if ((status = nc_create(NEW_FILE_NAME, NC_NETCDF4, &new_ncid))) {
+    printf("Error creating file: %s\n", nc_strerror(status));
+    return 1;
+  }
+
+  // TODO: Define the dimensions in the new file
+  // for (int i = 0; i < 6; i++) {
+  //
   // }
 
-  // For each variable
-  // for (int i = 0; i < var_names.size(); i++) {
-  //   // Get the variable id
-  //   if ((status = nc_inq_varid(ncid[0], var_names[i].c_str(), &varid[i]))) {
-  //     printf("Error getting variable id: %s\n", nc_strerror(status));
-  //     return 1;
-  //   }
-  //
-  //   // Read the data
-  //   // NOTE: nc_get_vara_float needs to be updated to handle vectors of data
+  // Define the dimensions in the new file
+  // if ((status = nc_def_dim(new_ncid, "dim1", DIM1, &dimids[0])) ||
+  // (status = nc_def_dim(new_ncid, "dim2", DIM2, &dimids[1]))) {
+  // printf("Error defining dimensions: %s\n", nc_strerror(status));
+  // return 1;
   // }
-  //
-  // // Close the old file
-  // if ((status = nc_close(ncid[0]))) {
-  //   printf("Error closing file: %s\n", nc_strerror(status));
-  //   return 1;
-  // }
-  //
-  // // Create the new file
-  // if ((status = nc_create(NEW_FILE_NAME, NC_NETCDF4, &new_ncid[0]))) {
-  //   printf("Error creating file: %s\n", nc_strerror(status));
-  //   return 1;
-  // }
-  //
-  // // Define the dimensions in the new file
-  // for (int i = 0; i < dim_names.size(); i++) {
-  //   if ((status =
-  //            nc_def_dim(new_ncid[0], dim_names[i].c_str(), DIM1,
-  //            &dimids[i]))) {
-  //     printf("Error defining dimensions: %s\n", nc_strerror(status));
-  //     return 1;
-  //   }
-  // }
-  //
-  // // Define the variables in the new file
-  // for (int i = 0; i < var_names.size(); i++) {
-  //   if ((status = nc_def_var(new_ncid[0], var_names[i].c_str(), NC_FLOAT,
-  //                            dim_names.size(), dimids.data(),
-  //                            &new_varid[i]))) {
-  //     printf("Error defining variable: %s\n", nc_strerror(status));
-  //     return 1;
-  //   }
-  //
-  //   // Set the filter
-  //   if ((status = nc_def_var_filter(new_ncid[0], new_varid[i], filter_id, 0,
-  //                                   params.data()))) {
-  //     printf("Error setting filter: %s\n", nc_strerror(status));
-  //     return 1;
-  //   }
-  //
-  //   // Write the data
-  //   // NOTE: nc_put_vara_float needs to be updated to handle vectors of data
-  // }
-  //
-  // // Close the new file
+
+  for (int i = 0; i < nvars; i++) {
+    if ((status = nc_def_var_filter(new_ncid, new_varid, SZ_FILTER, 0, 0))) {
+      printf("Error setting filter: %s\n", nc_strerror(status));
+      return 1;
+    }
+  }
+
+  // TODO: write the data
+
+  // Close the new file
   if ((status = nc_close(ncid))) {
     printf("Error closing file: %s\n", nc_strerror(status));
     return 1;
